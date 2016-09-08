5f7b8498c0d86bea7caff2d82e4163b4ff6db818
DavidPennington <DavidPennington@wifi-roaming-128-4-93-244.host.udel.edu>
2016-09-08 16:12:24 -0400

    First Commit

--- RioBLL/src/main/java/pokerBase/Card.java
+++ RioBLL/src/main/java/pokerBase/Card.java
@@ -3,8 +3,9 @@ package pokerBase;
 import java.util.Comparator;
 
 public class Card {
-
 	
+	private String[] rank = {"A", "2", "3","4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	private String[] suit = {"Diamonds", "Hearts", "Clubs", "Spades"};
 	
 	
 }
--- RioBLL/src/main/java/pokerBase/Deck.java
+++ RioBLL/src/main/java/pokerBase/Deck.java
@@ -4,6 +4,27 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck  {
-
-
+	 
+	
+	
+	public static void main(String[] args){
+	
+		int[] cards = new int[52];
+	
+		// Initialize the deck
+	
+		for(int c = 0; c < cards.length; c++)
+			cards[c]= c;
+			
+		//Shuffling the deck of cards
+		
+		for(int x = 0; x < cards.length; x++){
+			int order = (int)(Math.random() * cards.length);
+			int origVal = cards[x];
+			cards[x] = cards[order];
+			cards[order] = origVal;		
+		}
+		
+	
+}
 }
