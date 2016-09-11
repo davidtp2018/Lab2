8d21741bf21e8348457c3e1d43a85835e9c166eb
DavidPennington <DavidPennington@wifi-roaming-128-4-88-93.host.udel.edu>
2016-09-11 16:14:03 -0400

    Commit

--- RioBLL/src/main/java/pokerBase/Card.java
+++ RioBLL/src/main/java/pokerBase/Card.java
@@ -4,8 +4,34 @@ import java.util.Comparator;
 
 public class Card {
 	
-	private String[] rank = {"A", "2", "3","4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-	private String[] suit = {"Diamonds", "Hearts", "Clubs", "Spades"};
+	private int cardNumber;
+	private String suit;
+	private String rank;
+	private Hand Number;
+	private Deck deckNumber;
+	private String[] ranks = {"A", "2", "3","4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+	private String[] suits = {"Diamonds", "Hearts", "Clubs", "Spades"};
 	
+	public Card(int cardNumber){
+		this.cardNumber = cardNumber;
+		this.suit = suits[cardNumber/13];
+		this.rank = ranks[cardNumber % 13];
+	}
+	
+	public double getCardNumber(){
+		return cardNumber;
+	}
+	
+	public String getRank(){
+		return rank;
+	}
+	
+	public String getCardSuit(){
+		return suit;
+	}
+	
+	public String toString(){
+		return rank + " " + "of" + " " + suit;
+	}
 	
 }
--- RioBLL/src/main/java/pokerBase/Deck.java
+++ RioBLL/src/main/java/pokerBase/Deck.java
@@ -3,28 +3,46 @@ package pokerBase;
 import java.util.ArrayList;
 import java.util.Collections;
 
-public class Deck  {
-	 
+public class Deck {
 	
-	
-	public static void main(String[] args){
-	
-		int[] cards = new int[52];
-	
-		// Initialize the deck
-	
-		for(int c = 0; c < cards.length; c++)
-			cards[c]= c;
-			
-		//Shuffling the deck of cards
-		
-		for(int x = 0; x < cards.length; x++){
-			int order = (int)(Math.random() * cards.length);
-			int origVal = cards[x];
-			cards[x] = cards[order];
-			cards[order] = origVal;		
+	private static ArrayList<Card> cardList = new ArrayList<Card>();
+
+	// Initialize the deck
+
+	public static void initializeDeck(){
+		for(int i = 0; i < 52; i++){
+			cardList.add(new Card(i));
 		}
-		
-	
-}
+			}
+
+	  //Shuffling the deck of cards
+	  
+	  public static void shuffleCards(){ 
+		  java.util.Collections.shuffle(cardList);
+	  }
+		  
+	  
+	  // Drawling a card from the deck method
+	  
+	 public static Card drawCard(){
+		 return cardList.remove(0);
+	 }
+	 
+	 // Displaying the total amount of cards that are left in the deck
+	 
+	 public static int cardsLeft(){
+		 return cardList.size();
+	 }
+	 
+	  
+	 
+	public static void main(String[] args) {
+		initializeDeck();
+		System.out.println(cardList.size());
+		shuffleCards();
+		System.out.println(cardList);
+		System.out.println(drawCard());
+		System.out.println(cardsLeft());
+	}
+
 }
